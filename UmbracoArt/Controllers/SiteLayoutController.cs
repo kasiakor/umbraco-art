810c819000b7416d546ea980aac290dca208ef4f
Kasia Kordys <kasia.kordys@gmail.com>
2020-02-20 17:38:27 +0000

    add header partial view and move there header content

---
+++ UmbracoArt/Controllers/SiteLayoutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoArt.Controllers
+{
+    public class SiteLayoutController : SurfaceController
+    {
+        public ActionResult RenderHeader()
+        {
+            return PartialView("~/Views/Partials/SiteLayout/Header.cshtml");
+        }      
+    }
+}
