3d68601462ba51fd36558903cfff5178f95b227b
Kasia Kordys <kasia.kordys@gmail.com>
2020-02-20 17:59:47 +0000

    add footer partial view and move there footer content

--- UmbracoArt/Controllers/SiteLayoutController.cs
+++ UmbracoArt/Controllers/SiteLayoutController.cs
@@ -9,9 +9,15 @@ namespace UmbracoArt.Controllers
 {
     public class SiteLayoutController : SurfaceController
     {
+        private const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/SiteLayout/";
         public ActionResult RenderHeader()
         {
-            return PartialView("~/Views/Partials/SiteLayout/Header.cshtml");
-        }      
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Header.cshtml");
+        }
+
+        public ActionResult RenderFooter()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Footer.cshtml");
+        }
     }
 }
