33a999be152bbe5b6091b9a73228e9f9a55e71c4
Kasia Kordys <kasia.kordys@gmail.com>
2020-02-21 13:17:53 +0000

    add intro partial view and move there intro content

--- UmbracoArt/Controllers/SiteLayoutController.cs
+++ UmbracoArt/Controllers/SiteLayoutController.cs
@@ -15,6 +15,11 @@ namespace UmbracoArt.Controllers
             return PartialView(PARTIAL_VIEW_FOLDER + "_Header.cshtml");
         }
 
+        public ActionResult RenderIntro()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Intro.cshtml");
+        }
+
         public ActionResult RenderFooter()
         {
             return PartialView(PARTIAL_VIEW_FOLDER + "_Footer.cshtml");
