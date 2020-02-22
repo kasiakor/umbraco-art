cb2af52885f1c9999eb84be18f2227023e61b1a3
Kasia Kordys <kasia.kordys@gmail.com>
2020-02-22 17:16:07 +0000

    add caching to navigation

--- UmbracoArt/Controllers/SiteLayoutController.cs
+++ UmbracoArt/Controllers/SiteLayoutController.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using Umbraco.Core.Models;
 using Umbraco.Web.Mvc;
 using UmbracoArt.Models;
+using Umbraco.Core.Models;
+using System.Runtime.Caching;
 
 
 namespace UmbracoArt.Controllers
@@ -31,7 +30,12 @@ namespace UmbracoArt.Controllers
         /// 
         public ActionResult RenderHeader()
         {
-            List< NavigationListItem > nav = GetNavigationModelFromDatabase();
+            //List< NavigationListItem > nav = GetNavigationModelFromDatabase();
+            //isntead of getting object from the db get it from cache
+            //5min to stay in cache before expires
+            //name mainNav to give it in the cache
+            //GetNavigationModelFromDatabase the function to call in it doesnt exist in the cache
+            List<NavigationListItem> nav = GetObjectFromCache<List<NavigationListItem>>("mainNav", 5, GetNavigationModelFromDatabase);
             return PartialView(PARTIAL_VIEW_FOLDER + "_Header.cshtml", nav);
         }
 
@@ -71,6 +75,27 @@ namespace UmbracoArt.Controllers
                 }
             }
             return listItems;
+        }
+        /// <summary>
+        /// A generic function for getting and setting objects to the memory cache.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to be returned.</typeparam>
+        /// <param name="cacheItemName">The name to be used when storing this object in the cache.</param>
+        /// <param name="cacheTimeInMinutes">How long to cache this object for.</param>
+        /// <param name="objectSettingFunction">A parameterless function to call if the object isn't in the cache and you need to set it.</param>
+        /// <returns>An object of the type you asked for</returns>
+        private static T GetObjectFromCache<T>(string cacheItemName, int cacheTimeInMinutes, Func<T> objectSettingFunction)
+        {
+            ObjectCache cache = MemoryCache.Default;
+            var cachedObject = (T)cache[cacheItemName];
+            if (cachedObject == null)
+            {
+                CacheItemPolicy policy = new CacheItemPolicy();
+                policy.AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(cacheTimeInMinutes);
+                cachedObject = objectSettingFunction();
+                cache.Set(cacheItemName, cachedObject, policy);
+            }
+            return cachedObject;
         }
     }
 }
