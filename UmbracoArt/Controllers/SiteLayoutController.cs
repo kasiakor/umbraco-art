6d6b23b072c7ae29c9e41a1617e08b5915a05f8a
Kasia Kordys <kasia.kordys@gmail.com>
2020-02-22 16:48:04 +0000

    add dynamic nav

--- UmbracoArt/Controllers/SiteLayoutController.cs
+++ UmbracoArt/Controllers/SiteLayoutController.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Umbraco.Core.Models;
 using Umbraco.Web.Mvc;
+using UmbracoArt.Models;
+
 
 namespace UmbracoArt.Controllers
 {
     public class SiteLayoutController : SurfaceController
     {
         private const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/SiteLayout/";
-        public ActionResult RenderHeader()
-        {
-            return PartialView(PARTIAL_VIEW_FOLDER + "_Header.cshtml");
-        }
 
         public ActionResult RenderIntro()
         {
@@ -23,6 +22,55 @@ namespace UmbracoArt.Controllers
         public ActionResult RenderFooter()
         {
             return PartialView(PARTIAL_VIEW_FOLDER + "_Footer.cshtml");
+        }
+
+        /// <summary>
+        /// Renders the top navigation in the header partial
+        /// </summary>
+        /// <returns>Partial view with a model</returns>
+        /// 
+        public ActionResult RenderHeader()
+        {
+            List< NavigationListItem > nav = GetNavigationModelFromDatabase();
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Header.cshtml", nav);
+        }
+
+
+        /// <summary>
+        /// Finds the home page and gets the navigation structure based on it and it's children
+        /// </summary>
+        /// <returns>A List of NavigationListItems, representing the structure of the site.</returns>
+        private List<NavigationListItem> GetNavigationModelFromDatabase()
+        {
+            const int HOME_PAGE_POSITION_IN_PATH = 1;
+            int homePageId = int.Parse(CurrentPage.Path.Split(',')[HOME_PAGE_POSITION_IN_PATH]);
+            IPublishedContent homePage = Umbraco.Content(homePageId);
+            List<NavigationListItem> nav = new List<NavigationListItem>();
+            nav.Add(new NavigationListItem(new NavigationLink(homePage.Url, homePage.Name)));
+            nav.AddRange(GetChildNavigationList(homePage));
+            return nav;
+        }
+
+        /// <summary>
+        /// Loops through the child pages of a given page and their children to get the structure of the site.
+        /// </summary>
+        /// <param name="page">The parent page which you want the child structure for</param>
+        /// <returns>A List of NavigationListItems, representing the structure of the pages below a page.</returns>
+        private List<NavigationListItem> GetChildNavigationList(dynamic page)
+        {
+            List<NavigationListItem> listItems = null;
+            var childPages = page.Children.Where("Visible");
+            if (childPages != null && childPages.Any() && childPages.Count() > 0)
+            {
+                listItems = new List<NavigationListItem>();
+                foreach (var childPage in childPages)
+                {
+                    NavigationListItem listItem = new NavigationListItem(new NavigationLink(childPage.Url, childPage.Name));
+                    listItem.Items = GetChildNavigationList(childPage);
+                    listItems.Add(listItem);
+                }
+            }
+            return listItems;
         }
     }
 }
