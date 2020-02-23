1c920c369a7ab52a8984ba3772d62894a6b68ae8
Kasia Kordys <kasia.kordys@gmail.com>
2020-02-23 16:54:46 +0000

    add code to handle empty title

--- UmbracoArt/App_Data/Models/all.generated.cs
+++ UmbracoArt/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "da9d2fcb7a46dcd0")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
